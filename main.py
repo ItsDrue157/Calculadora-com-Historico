eaedccb2ab3f9e01091bd27fea5273c63e8e28a1
Borges <carlosmateus7720@gmail.com>
2025-01-22 00:42:25 -0300

    adicionado o metodo de divisao operacao de divisao implementada em codigo 0

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ resultado = []
 def Exibir_menu():
     Limpar_tela()
     escolha = input('''
-Hello!!!, SEJA-BEMVINDO
+Hello!!!, SEJA BEM VINDO
                     
 Digite 1 para fazer operacoes pelo metodo de adicao: 
 Digite 2 para fazer operacoes pelo metodo de subtracao: 
@@ -19,6 +19,8 @@ digite 3 para fazer operacoes pelo metodo de divisao: (nao funcionando ainda)
                 Fazer_operacao_Adicao()
             case '2':
                 Fazer_operacao_subtracao()
+            case '3':
+                Fazer_operacao_divisao()
 
 
 
@@ -58,6 +60,33 @@ def Fazer_operacao_subtracao():
     print(f"Resultado da subtracao: {resultado_subtracao}")
     
     Escrever_no_historico(resultado_subtracao)
+    
+    escolha_voltar_menu = input("Deseja voltar ao menu? s/n ")
+    if escolha_voltar_menu == 's':
+        Voltar_ao_menu() 
+    else:
+        exit()
+    
+def Fazer_operacao_divisao():
+    Limpar_tela()
+    numero1 = float(input("Digite o primeiro numero: "))
+    
+    numero2 = float(input("Digite o segundo numero: "))
+    
+    if numero2 == 0:
+        print("nenhum numero pode ser dividido por 0")
+        print('Voltando ao menu')
+        time.sleep(2)
+        Exibir_menu()
+    elif numero1 and numero2 == 0:
+        print('O resultado foi 1, pois 0 divido por 0, resulta em 1')
+        return
+    
+    resultado_divisao = numero1 / numero2
+    
+    print(f"Resultado da divisao: {resultado_divisao}")
+    
+    Escrever_no_historico(resultado_divisao)
     
     escolha_voltar_menu = input("Deseja voltar ao menu? s/n ")
     if escolha_voltar_menu == 's':
